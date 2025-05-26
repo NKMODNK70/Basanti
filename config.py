bf1bc647c5d5d0ba38625719633c0053384a3392
NKMODNK70 <noj505546@gmail.com>
2025-05-26 17:18:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,8 +16,6 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", Jai) # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
 
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
