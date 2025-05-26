48b52ea8610c7c334330bb4805246ae483cd137a
NKMODNK70 <noj505546@gmail.com>
2025-05-26 16:43:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", None) # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
+API_KEY = getenv("API_KEY", Jai) # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
 
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
